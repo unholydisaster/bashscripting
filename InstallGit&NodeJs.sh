b5b27febc829c4e777759adb09f42e4cca0fab93
Eli musa <60873440+unholydisaster@users.noreply.github.com>
2023-03-17 10:28:43 +0000

    Create InstallGit&NodeJs.sh

---
+++ InstallGit&NodeJs.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Update the package manager
+sudo apt update
+
+# Install curl
+sudo apt install curl
+
+# Install Git
+sudo apt install git -y
+
+# Verify the installation version
+git --version
+
+# Install nvm(Nodejs Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+source ~/.bashrc
+nvm --version
+
+# Install Node.js using nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source ~/.bashrc
+nvm install node
+
+# Verify the installation version
+node --version
+
+# Instal snapd
+sudo apt install snapd
+
+# Install and download visual studio code
+sudo snap install --classic code
+
+# Cone an existing repository
+git clone https://github.com/unholydisaster/my-markdown
+
+# Navigate to working space and install dependency packages
+cd my-markdown
+npm install
+
+# Then open the working directory
+code .
